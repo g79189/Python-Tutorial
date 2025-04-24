8ebd872f6fe1c096fa2dc48c6d38739494f77df8
g79189 <greg.jones.nc@gmail.com>
2025-04-24 18:48:44 -0400

    Create List.py

---
+++ List.py
@@ -0,0 +1,10 @@
+fruit=["orange","grapes","banana","cherry"]
+
+fruit.insert(1,"apple")
+print (fruit)   
+fruit.remove("grapes")
+print (fruit)
+fruit.sort()
+print(fruit)
+fruit.reverse()
+print(fruit)
