8da5eeabe25d6a87fe6729d9fa8b724b154cfa90
g79189 <greg.jones.nc@gmail.com>
2025-05-15 18:46:41 -0400

    Update string_replacement_examples.py

--- week4/string_replacement_examples.py
+++ week4/string_replacement_examples.py
@@ -1,1 +1,6 @@
-
+text="Hello, World!"
+new_text=text.replace("World","Python")
+print(new_text)  # Output: Hello, Python!
+text="apple apple apple"
+new_text = text.replace("apple","orange",2)
+print(new_text)  # Output: orange orange apple
