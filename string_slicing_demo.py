5468a5cb24a0f0b55dee746beae74e6d21c33594
g79189 <greg.jones.nc@gmail.com>
2025-05-15 18:35:10 -0400

    Create string_slicing_demo.py

---
+++ string_slicing_demo.py
@@ -0,0 +1,13 @@
+text="Python Programming"
+slice1 = text[0:6]
+print(slice1)  # Output: Python
+slice2 = text[7:18]
+print(slice2)  # Output: Programming
+slice3 = text[:6]
+print(slice3)  # Output: Python
+slice4 = text[7:]
+print(slice4)  # Output: Programming    
+slice5 = text[-11:]
+print(slice5)  # Output: Programming
+slice6 = text[:-12]
+print(slice6)  # Output: Python
